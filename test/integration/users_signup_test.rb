030737a850f0fc99e6b5298e7d682b33c712c8c8
Mark Tice <marktice@protonmail.com>
2018-04-09 14:47:33 +1000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,13 +6,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post signup_path, params: { user: { name: "",
+                                          email: "user@invalid",
+                                          password:              "foo",
+                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name:  "valid user",
+                                          email: "user@valid.com",
+                                          password:              "validpassword",
+                                          password_confirmation: "validpassword" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
